ebaf59a54d9a672d826c41c4ea5b5df8ad32bbf6
gogo <gogo246475@gmail.com>
2022-01-03 16:06:00 +0100

    build logo and about button

--- rgui/src/window_open_file.rs
+++ rgui/src/window_open_file.rs
@@ -1,23 +1,54 @@
 extern crate druid;
-use self::druid::{AppLauncher, WindowDesc, Widget, PlatformError, Data, Env, Lens, LocalizedString, WidgetExt};
-use self::druid::widget::{Align, Flex, Label, TextBox};
+use self::druid::{AppLauncher, WindowDesc, Widget, PlatformError, Data, 
+                  Lens, LocalizedString, UnitPoint, ImageBuf
+                  };
+                  
+use self::druid::widget::{Align, Image, Flex, Button, FillStrat};
+
+/// # WINDOW_TITLE
+///
+/// Title of the application.
 
 const WINDOW_TITLE: LocalizedString<HelloState> = LocalizedString::new("Open the file");
+
+/// # HelloState
+///
+/// State of the application.
 
 #[derive(Clone, Data, Lens)]
 pub struct HelloState {
     name: String,
 }
 
+/// # build_flex_logo_and_about
+///
+/// Builds the flex with the rair log and the about button.
+
+fn build_flex_logo_and_about() -> Flex <HelloState> {
+    Flex::column()
+        .with_child(build_logo())
+        .with_child(Button::new("about"))
+}
+
+/// # build_logo
+///
+/// Builds the rair logo (currently the odcoder png)
+
+fn build_logo() -> impl Widget<HelloState> {
+    let logo_oddcoder = ImageBuf::from_data(include_bytes!("../data/oddcoder.png")).unwrap();
+    let img = Image::new(logo_oddcoder).fill_mode(FillStrat::Fill);
+    let centered = Align::horizontal(UnitPoint::TOP, img);
+    centered
+}
+
 /// # build_windows_to_open_binary
 ///
 /// Builds a label with the text passed as argument.
 
-pub fn build_windows_to_open_binary() -> WindowDesc<HelloState> {
-    let label = Label::new("Hello world");
-    let main_window = WindowDesc::new(label)
+fn build_windows_to_open_binary() -> WindowDesc<HelloState> {
+    let main_window = WindowDesc::new(build_flex_logo_and_about)
         .title(WINDOW_TITLE)
-        .window_size((400.0, 400.0));
+        .window_size((600.0, 700.0));
     main_window
 }
 
@@ -26,12 +57,10 @@ pub fn build_windows_to_open_binary() -> WindowDesc<HelloState> {
 /// Builds everything.
 
 pub fn build_main_gui() -> Result<(), PlatformError> {
-    
     // create the initial app state
     let initial_state = HelloState {
         name: "World".into(),
     };
-    
     
     let start_up_windows = build_windows_to_open_binary();
     AppLauncher::with_window(start_up_windows).launch(initial_state)?;
